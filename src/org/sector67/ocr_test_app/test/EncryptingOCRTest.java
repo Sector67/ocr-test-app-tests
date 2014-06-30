a38d97e825f5cb9d8d7014324b441817c47262b7
sphasse <scott.hasse@gmail.com>
2014-06-30 00:12:54 -0500

    Added zxing as a lib, new test cases

--- src/org/sector67/ocr_test_app/test/EncryptingOCRTest.java
+++ src/org/sector67/ocr_test_app/test/EncryptingOCRTest.java
@@ -1,5 +1,6 @@
 package org.sector67.ocr_test_app.test;
 
+import java.util.Arrays;
 import java.util.Random;
 
 import org.sector67.otp.EncryptionException;
@@ -7,6 +8,7 @@ import org.sector67.otp.cipher.OneTimePadCipher;
 import org.sector67.otp.key.InMemoryKeyStore;
 import org.sector67.otp.key.KeyException;
 import org.sector67.otp.utils.BaseUtils;
+import org.sector67.otp.utils.ErrorCorrectingUtils;
 
 import android.graphics.Bitmap;
 import android.graphics.Canvas;
@@ -18,7 +20,11 @@ import android.os.Environment;
 import android.test.AndroidTestCase;
 import android.util.Log;
 
+import com.googlecode.leptonica.android.Pix;
+import com.googlecode.leptonica.android.Pixa;
+import com.googlecode.tesseract.android.ResultIterator;
 import com.googlecode.tesseract.android.TessBaseAPI;
+import com.googlecode.tesseract.android.TessBaseAPI.PageIteratorLevel;
 
 public class EncryptingOCRTest extends AndroidTestCase {
 
@@ -131,6 +137,134 @@ public class EncryptingOCRTest extends AndroidTestCase {
 		assertEquals("The OCR'd text did not match the image", original, decrypted);
 
 
+	}
+
+	
+	public void testRandomMessagesWithWordForcing() throws Exception {
+		Log.e(TAG, "Data path is: " + DATA_PATH + "");
+		//consistent seed for repeatable tests
+		Random r = new Random(2);
+
+		// encrypt some data
+		for(int i = 0 ; i < 10; i++) {
+			byte[] starting = new byte[20];
+			r.nextBytes(starting);
+			byte[] ecc = ErrorCorrectingUtils.encode(starting);
+			String chunked = BaseUtils.getChunkedBase16(ecc);
+			Log.i(TAG, "chunked output: " + chunked);
+
+			//make an image out of it
+		
+			Bitmap bitmap = OCRTestUtils.getTextImage(chunked, 24.0f, 2048, 480);
+		
+			//OCR the image
+		
+			TessBaseAPI baseApi = new TessBaseAPI();
+			baseApi.setDebug(true);
+			baseApi.init(DATA_PATH, lang);
+			baseApi.ReadConfigFile("config.txt");
+			//limit the characters to hex values
+			baseApi.setVariable(TessBaseAPI.VAR_CHAR_WHITELIST, "0123456789ABCDEF");
+			baseApi.setVariable("language_model_penalty_non_freq_dict_word", "0.9");
+			baseApi.setVariable("language_model_penalty_non_dict_word", "0.9");
+			baseApi.setImage(bitmap);
+			
+			String recognizedText = baseApi.getUTF8Text();
+			
+			recognizedText = "";
+			final ResultIterator iterator = baseApi.getResultIterator();
+			String lastUTF8Text;
+			float lastConfidence;
+			int count = 0;
+			iterator.begin();
+			do {
+			    lastUTF8Text = iterator.getUTF8Text(PageIteratorLevel.RIL_WORD);
+			    if (lastUTF8Text.length() == 3) {
+			    	lastUTF8Text = "FF";
+			    }
+			    recognizedText = recognizedText + lastUTF8Text + ":";
+			    lastConfidence = iterator.confidence(PageIteratorLevel.RIL_WORD);
+			    count++;
+			} while (iterator.next(PageIteratorLevel.RIL_WORD));
+			
+			
+			baseApi.end();
+	
+			Log.i(TAG, "chunked input: " + recognizedText);
+			String cleanedText = recognizedText.replaceAll("[^a-fA-F0-9]+", "");
+
+			assertTrue("A non even amount of characters was recognized [" + chunked + " ] [" + recognizedText + "]", cleanedText.length() % 2 == 0);
+
+			try {
+			//decrypt the text
+			byte[] ending = BaseUtils.base16ToBytes(cleanedText);
+			
+			byte[] errorCorrected = ErrorCorrectingUtils.decode(ending);
+			
+			assertTrue("The OCR'd text did not match the original [" + chunked + " ] [" + recognizedText + "]", Arrays.equals(starting, errorCorrected));
+			} catch (Exception e) {
+				fail("An unexpected exception occured [" + chunked + " ] [" + recognizedText + "] " + e.getMessage());
+				throw e;
+			}
+		}
+	}
+	
+	
+	public void testRandomMessages() throws Exception {
+		Log.e(TAG, "Data path is: " + DATA_PATH + "");
+		//consistent seed for repeatable tests
+		Random r = new Random(3);
+
+		// encrypt some data
+		for(int i = 0 ; i < 10; i++) {
+			byte[] starting = new byte[20];
+			r.nextBytes(starting);
+			
+			byte[] ecc = ErrorCorrectingUtils.encode(starting);
+			//byte[] ecc = starting;
+			
+			String chunked = BaseUtils.getChunkedBase16(ecc);
+			Log.i(TAG, "chunked output: " + chunked);
+
+			//make an image out of it
+		
+			Bitmap bitmap = OCRTestUtils.getTextImage(chunked, 24.0f, 2048, 480);
+		
+			//OCR the image
+		
+			TessBaseAPI baseApi = new TessBaseAPI();
+			baseApi.setDebug(true);
+			baseApi.init(DATA_PATH, lang);
+			baseApi.ReadConfigFile("config.txt");
+			//limit the characters to hex values
+			baseApi.setVariable(TessBaseAPI.VAR_CHAR_WHITELIST, "0123456789ABCDEF");
+			baseApi.setVariable("language_model_penalty_non_freq_dict_word", "0.9");
+			baseApi.setVariable("language_model_penalty_non_dict_word", "0.9");
+			baseApi.setImage(bitmap);
+			
+			String recognizedText = baseApi.getUTF8Text();
+			
+			
+			baseApi.end();
+	
+			Log.i(TAG, "chunked input: " + recognizedText);
+			String cleanedText = recognizedText.replaceAll("[^a-fA-F0-9]+", "");
+
+			assertTrue("A non even amount of characters was recognized [" + chunked + " ] [" + recognizedText + "]", cleanedText.length() % 2 == 0);
+
+			try {
+			//decrypt the text
+			byte[] ending = BaseUtils.base16ToBytes(cleanedText);
+			
+			byte[] errorCorrected = ErrorCorrectingUtils.decode(ending);
+			//byte[] errorCorrected = ending;
+			
+			assertTrue("The OCR'd text did not match the original [" + chunked + " ] [" + recognizedText + "]", Arrays.equals(starting, errorCorrected));
+			} catch (Exception e) {
+				fail("An unexpected exception occured [" + chunked + " ] [" + recognizedText + "] " + e.getMessage());
+				throw e;
+			}
+		}
 	}
 
 }
--- src/org/sector67/ocr_test_app/test/OCRTestUtils.java
+++ src/org/sector67/ocr_test_app/test/OCRTestUtils.java
@@ -6,13 +6,17 @@ import android.graphics.Color;
 import android.graphics.Paint;
 import android.graphics.Paint.Align;
 import android.graphics.Paint.Style;
+import android.graphics.Typeface;
 
 public class OCRTestUtils {
 	
+    public static Bitmap getTextImage(String text, int width, int height) {
+    	return getTextImage(text, 24.0f, width, height);
+    }
 	/*
 	 * Make a dynamic test image
 	 */
-    public static Bitmap getTextImage(String text, int width, int height) {
+    public static Bitmap getTextImage(String text, float size, int width, int height) {
         final Bitmap bmp = Bitmap.createBitmap(width, height, Bitmap.Config.ARGB_8888);
         final Paint paint = new Paint();
         final Canvas canvas = new Canvas(bmp);
@@ -23,7 +27,8 @@ public class OCRTestUtils {
         paint.setStyle(Style.FILL);
         paint.setAntiAlias(true);
         paint.setTextAlign(Align.CENTER);
-        paint.setTextSize(24.0f);
+        paint.setTextSize(size);
+        paint.setTypeface(Typeface.DEFAULT);
         canvas.drawText(text, width / 2, height / 2, paint);
 
         return bmp;
